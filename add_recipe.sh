3273ac0d14bd7909eeb38185f16c7a16e2323b7f
Kasper Grubbe <kawsper@gmail.com>
2017-07-12 14:41:07 +0100

    Let us be a nice terminal script does exit codes

--- add_recipe.sh
+++ add_recipe.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function cleanup()
+{
+  echo "Cleaning up"
+  rm -rf ey-cookbooks-stable-v5
+}
+
 echo 'Downloading v5 chef repository'
 git clone https://github.com/engineyard/ey-cookbooks-stable-v5
 
@@ -10,9 +16,10 @@ if [ -d ey-cookbooks-stable-v5/custom-cookbooks/$1/cookbooks/custom-$1 ]; then
   echo "Adding $1 recipe"
   echo "include_recipe 'custom-$1'" >> ./cookbooks/ey-custom/recipes/after-main.rb
   echo "depends 'custom-$1'" >> ./cookbooks/ey-custom/metadata.rb
+  cleanup
+  exit 0
 else
   echo "! NO SUCH RECIPE AS $1"
+  cleanup
+  exit 1
 fi
-
-echo "Cleaning up"
-rm -rf ey-cookbooks-stable-v5
